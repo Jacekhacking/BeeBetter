6baff4c42a3c222779f5db31ce1423f3196baf04
jacekhacking <Jacekhacking@mac.com>
2022-05-09 21:50:02 -0600

    logos changed to match favicon

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -20,7 +20,11 @@ const Navigation = (props) => {
 
 
                 <div className={`flex`} style={{justifyContent:'center'}}>
-                    <a href="https://www.vagaro.com/beebettermassage" target={'_blank'} rel={"noopener noreferrer"}><h2 className={`${styles.bookingLink} fs-500`} style={{textAlign: 'center'}}>Book A Session</h2></a>
+                    <a href="https://www.vagaro.com/beebettermassage"
+                       target={'_blank'}
+                       rel={"noopener noreferrer"}><h2
+                        className={`${styles.bookingLink} fs-500`}
+                        style={{textAlign: 'center'}}>Book A Session</h2></a>
                 </div>
 
 
