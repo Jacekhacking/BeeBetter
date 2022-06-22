a9f44472a8c0481609f53439cfaf8b0e0de2fa81
jacekhacking <Jacekhacking@mac.com>
2022-06-21 22:55:58 -0600

    completed mvp of reviews

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -7,17 +7,27 @@ const Reviews = () => {
 
 
     return (
-        <div id={"Reviews"} className={styles['reviews-container']}>
+        <div className={'flex'} style={{justifyContent:'center', alignItems:'center'}}>
+            <div id={"Reviews"} className={styles['reviews-container']}>
+                {reviewsData.map(review => (
+                    <div className={styles['review-item']}>
+                        <div className={styles['review-header']}>
+                            <div>
+                                <h2 className={'fs-400'}>{review.Name} <span className={'fs-200'}>{review.Date}</span></h2>
+                            </div>
+                            <div>
 
-            {reviewsData.map(review => (
-                <div className={styles['review-item']}>
-                    <div>
-                        <h2 className={'fs-400'}>{review.Name} <span className={'fs-200'}>{review.Date}</span></h2>
+                            </div>
+                        </div>
+
+                        <div className={styles['review-text']}>{review.Review}</div>
                     </div>
-                    <div className={styles['review-text']}>{review.Review}</div>
-                </div>
-            ))}
+                ))}
+            </div>
         </div>
+
+
+
     )
 }
 
